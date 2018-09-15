8f9f6020a64380bf97bdf51de8603cb7989dc7a8
zcjlq <11943098@qq.com>
2018-09-15 16:31:10 +0800

    临时(6-4重构用户名密码登陆)

--- ssm-security-app/src/main/java/com/ssm/security/app/authtication/MyAuthenticationSuccessHandler.java
+++ ssm-security-app/src/main/java/com/ssm/security/app/authtication/MyAuthenticationSuccessHandler.java
@@ -1,13 +1,20 @@
 package com.ssm.security.app.authtication;
 
-import com.ssm.security.core.properties.LoginType;
 import com.ssm.security.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -32,6 +39,10 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
 
     @Autowired
     private SecurityProperties securityProperties;
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
 
     /**
      * 用户登录成功到进此方法
@@ -47,13 +58,57 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
             throws IOException, ServletException {
         log.info("登陆成功 MyAuthenticationSuccessHandler:", ReflectionToStringBuilder.reflectionToString(authentication));
 
-        // 如果是json，返回json，否则是html调用父类处理逻辑
-        if (securityProperties.getBrowser().getLoginType().equals(LoginType.JSON)) {
-            // 登陆成功后，把用户信息写到响应中
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(request, response, authentication);
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
+
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在：" + clientId);
+        } else if (!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配:" + clientId);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException e) {
+            throw new BadCredentialsException(
+                    "Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[]{token.substring(0, delim), token.substring(delim + 1)};
     }
 }
--- ssm-security-app/src/main/java/com/ssm/security/app/authtication/SsmResourceServerConfig.java
+++ ssm-security-app/src/main/java/com/ssm/security/app/authtication/SsmResourceServerConfig.java
@@ -1,7 +1,16 @@
 package com.ssm.security.app.authtication;
 
+import com.ssm.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.ssm.security.core.properties.SecurityConstants;
+import com.ssm.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * 开启资源服务器
@@ -14,5 +23,48 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @Configuration
 @EnableResourceServer
-public class SsmResourceServerConfig {
+public class SsmResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer ssmSocialSecurityConfigurer;
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        {
+            http.formLogin()
+                    .loginPage(SecurityConstants.DEFAULT_LOGIN_PAGE_URL)
+                    .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                    .successHandler(authenticationSuccessHandler)
+                    .failureHandler(authenticationFailureHandler);
+//            .apply(validateCodeSecurityConfig)
+
+            http.apply(smsCodeAuthenticationSecurityConfig)
+                    .and()
+                    .apply(ssmSocialSecurityConfigurer)
+                    // 以下是记住我功能实现
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                            SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                            securityProperties.getBrowser().getLoginPage(),
+                            SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                            securityProperties.getBrowser().getSignUpUrl(),
+                            securityProperties.getBrowser().getSignOutUrl(),
+                            securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                            "/user/register")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()// 需要身份认证
+                    .and().csrf().disable();
+        }
+    }
 }

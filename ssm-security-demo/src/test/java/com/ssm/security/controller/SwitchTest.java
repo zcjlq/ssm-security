abbf1137b6fb369e0be859f9f2198690603f787e
zcjlq <zcjlq@me.com>
2020-12-28 20:21:49 +0800

    test switch

---
+++ ssm-security-demo/src/test/java/com/ssm/security/controller/SwitchTest.java
@@ -0,0 +1,23 @@
+package com.ssm.security.controller;
+
+import org.junit.Test;
+
+public class SwitchTest {
+
+    /**
+     * 测试不加break，将会打印 2 和 3
+     */
+    @Test
+    public void testCase() {
+        String s = "a";
+        switch (s) {
+            case "b":
+                System.out.println("----1");
+            case "a":
+                System.out.println("----2");
+            default:
+                System.out.println("----3");
+        }
+    }
+
+}
